681909f0c5dfb09b0ed6f6cefb1d8f081de86ba6
SeMbA Max <semba.max@gmail.com>
2019-01-15 02:46:34 +0200

    Initial project.

---
+++ app/src/main/java/com/semba/photoeditor/PhotoEditorActivity.kt
@@ -0,0 +1,12 @@
+package com.semba.photoeditor
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PhotoEditorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_editor)
+    }
+}
